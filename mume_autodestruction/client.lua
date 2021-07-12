694a7330e6042710843281c08071e77de47ebeb5
Mume <43515908+alejandromume@users.noreply.github.com>
2021-07-12 01:17:02 +0100

    Add files via upload

---
+++ mume_autodestruction/client.lua
@@ -0,0 +1,18 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand("autodestroy", function(source, args)
+    local jugador = GetPlayerPed(-1)
+    local x, y, z = table.unpack(GetEntityCoords(GetPlayersLastVehicle(jugador)))
+    
+    for i=5,1, -1 do
+        TriggerEvent('chat:addMessage', {
+            color = { 255, 0, 0},
+            multiline = true,
+            args = {"Autodestruction", i .. " seconds"}
+          })
+          Citizen.Wait(1000)
+    end
+
+    AddExplosion(x, y, z, 40, 2.0, true, false, true)
+end)
